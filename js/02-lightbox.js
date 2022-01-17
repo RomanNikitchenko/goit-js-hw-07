c41044998de110fc72647077b955edac7b94f9cf
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-17 16:48:17 +0200

    ht js 07 part 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const cardsMarcup = createImageCardsMarcup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", cardsMarcup);
+
+function createImageCardsMarcup(Items) {
+    return Items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join("");
+};
+
+gallery.addEventListener('click', onImageContainerclick);
+
+function onImageContainerclick(evt) {
+    evt.preventDefault();
+
+    // if (!evt.target.classList.contains("gallery__image")) {
+    //     return
+    // };
+
+    const ancestorSelector = evt.target.closest('.gallery__item');
+
+    console.log(ancestorSelector.getAttribute('href'));
+    console.log(evt.target)
+};
